9e8f2884282972472fd32660673f9b50302b8596
johnschneider1 <johnlawschneider@gmail.com>
2019-11-22 09:41:18 -0600

    rough finished

--- folio/src/landingPage.js
+++ folio/src/landingPage.js
@@ -3,31 +3,38 @@ import Navbar from "./Navbar";
 import Footer from "./footer";
 import "./landingpage.css";
 import { Icon } from "semantic-ui-react";
+import LiveAction from "./threeAni";
+import ReactRain from "react-rain-animation";
+import "react-rain-animation/lib/style.css";
 
 const landingPage = () => {
   return (
     <body>
       <section>
         <div className="mainbox">
+          
           <div className="header-box">
-            <div className="introduction-name">John Schneider=></div>
+            <div className="introduction-name">John Schneider</div>
             <div className="introduction-type">
               Options Trader and Software Engineer
             </div>
           </div>
-          <div className="description">
-            I am a veteran of the options and futures trading world and a Full
-            Stack Engineer <br /> My unique skill set alllows me to approach
-            problem solving from all possiple angles. <br /> React, React
-            Native, Node.js, Python, HTML, CSS, JavaScript, SQL....
+          <div className="cube" width={200} height={200}>
+            {/* <LiveAction className="cube-itself" /> */}
           </div>
+          <Icon name="mail" />
+
+          {/* <div className="description">
+            I am a veteran of the options and futures trading world and a Full
+            Stack EngineerMy unique skill set alllows me to approach problem
+            solving from all possiple angles. React, React Native, Node.js,
+            Python, HTML, CSS, JavaScript, SQL....
+          </div> */}
 
           <div className="connect">
             <div className="connect-text">Find Me Here</div>
             <a href="mailto:johnlawschneider@gmail.com">
-              <div className="email-connect">
-                Email <Icon name="mail" />
-              </div>
+              <div className="email-connect">Email</div>
             </a>
             <a href="https://github.com/johnschneider1">
               <div className="git-connect">Github</div>
