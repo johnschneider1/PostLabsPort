9eba2ba280e6abd550aa91e5dc9b336fa1e57a57
johnschneider1 <johnlawschneider@gmail.com>
2020-02-25 11:04:49 -0600

    changed some text

--- folio/src/Work.jsx
+++ folio/src/Work.jsx
@@ -37,8 +37,8 @@ const Work = () => {
           <p>
             I have always had a connection to the "markets" thus <br />
             my first build outside of Lambda is equity quotes and charting.
-            <br /> BrightLight is a react and node application, <br />
-            it's login process in hashed with bcrypt and secured with jwt.
+            <br /> BrightLight is a React and Node application, <br />
+            with login process in hashed with bcrypt and secured with jwt.
           </p>
           <a href="https://github.com/johnschneider1/BrightLight">
             <div className="linky">Code</div>
@@ -53,7 +53,7 @@ const Work = () => {
             possibilites of unique creation with.
             <br /> MUD is a multi user dungeon built with a team of programmers
             at Lambda.
-            <br /> It's a react frontend and django and python backend. <br />{" "}
+            <br /> It's a react frontend and Django and Python backend. <br />{" "}
             It includes a chat function built with "Pusher".
           </p>
           <a href="https://github.com/Team-Chaotic-Lamba/Frontend">
